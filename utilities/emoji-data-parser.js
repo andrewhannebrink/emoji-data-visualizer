a3fcee039f4db54f290559e5d6202ad6fd78c3fc
Andrew Hannebrink <brink@Andrews-MacBook-Air.local>
2016-10-07 23:05:02 -0700

    json loading error persists, dynamic unicode string handling function works

--- utilities/emoji-data-parser.js
+++ utilities/emoji-data-parser.js
@@ -2,9 +2,37 @@ const fs = require('fs'),
         lineReader = require('readline').createInterface({
             input: require('fs').createReadStream('./emoji-data.txt')
         }),
-        emojis = [];
+        graph = {
+            nodes: [],
+            links: []
+        };
+
+const connectAllNodes = (dataset) => {
+    for (let i = 0; i < dataset.nodes.length - 1; i += 1) {
+        for (let j = i + 1; j < dataset.nodes.length; j += 1) {
+            dataset.links.push({
+                source: i,
+                target: j
+            }); 
+        }   
+    }   
+}; 
+
+const connectSomeNodes = (graph, n = 200)  => {
+    for (let i = 0; i < n; i += 1) {
+        const n1 = Math.floor(Math.random() * graph.nodes.length),
+                n2 = Math.floor(Math.random() * graph.nodes.length);
+
+        graph.links.push({
+            source: n1,
+            target: n2
+        });
+    }
+};
 
 
+let done = false,
+        id = 0;
 lineReader.on('line', line => {
     // Not a comment
     if (line[0] !== '#' &&
@@ -23,16 +51,21 @@ lineReader.on('line', line => {
         }
         for (let i = 0; i < codeRange; i += 1) {
             emoji.code = code;
+            emoji.id = id;
             emoji.version = version;
             code = (parseInt('0x' + code) + 1).toString(16);
-            emojis.push(emoji);
+            id += 1;
+            graph.nodes.push(emoji);
             //console.log(emoji);
-            //console.log(emojis.length);
         }
-    } else if (line.indexOf('Emoji_Presentation') !== -1) {
-        const json = JSON.stringify(emojis);
+    } else if (line.indexOf('Emoji_Presentation') !== -1 &&
+            done === false) {
+        connectSomeNodes(graph);
+        const json = JSON.stringify(graph);
         fs.writeFile('emoji-data.json', json);
+        console.log(graph.links.length);
         lineReader.close();
+        done = true;
     }
 });
 
