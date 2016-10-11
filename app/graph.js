41c8454a40443fd44cb7aafc40407bcbd6a460b7
Andrew Hannebrink <brink@Andrews-MacBook-Air.local>
2016-10-10 19:07:24 -0700

    white to green to yellow to orange to red to black link weight color scheme now works

--- app/graph.js
+++ app/graph.js
@@ -2,15 +2,47 @@
 
     d3.json("/emoji-data.json", graph => {
 
+        const occurrencesToUnit = d3.scaleLinear()
+                .domain([0, d3.max(graph.links, d => d.occurrences)])
+                .range([0, 1]);
+
+        const occurrencesToOpacity = d3.scaleLinear()
+                .domain([0, d3.max(graph.links, d => d.occurrences)])
+                .range([0.2, 1]);
+
         const occurrencesToColor = d3.scaleLinear()
                 .domain([0, d3.max(graph.links, d => {
                     return d.occurrences;
                 })])
                 .range([0, 256]);
 
-        const occurrencesToUnit = d3.scaleLinear()
-                .domain([0, d3.max(graph.links, d => d.occurrences)])
-                .range([0, 1]);
+        const occurrencesToRgb = d => {
+            const totalRgbSteps = Math.floor(
+                            (256 * 4) * occurrencesToUnit(d.occurrences)
+                            ),
+                    fourth = totalRgbSteps / 256;
+            let r = 255,            
+                    g = 255,
+                    b = 255;
+
+            if (fourth < 1) {
+                r -= totalRgbSteps;
+                b -= totalRgbSteps;
+            } else if (fourth >= 1 && fourth < 2) {
+                b = 0;
+                g = 255;
+                r = (totalRgbSteps % 256);
+            } else if (fourth >= 2 && fourth < 3) {
+                b = 0;
+                g -= (totalRgbSteps % 256);
+            } else {
+                b = 0;
+                g = 0;
+                r -= (totalRgbSteps % 256);
+            }
+
+            return d3.rgb(r, g, b);
+        };
 
         const dragstarted = d => {
             if (!d3.event.active) {
@@ -49,14 +81,9 @@
                 .selectAll('line')
                 .data(graph.links)
                 .enter().append('line')
-                .attr('stroke-width', d => 5 * occurrencesToUnit(d.occurrences))
-                .attr('stroke-opacity', d => occurrencesToUnit(d.occurrences))
-                .attr('stroke', d => {
-                    return d3.rgb(255,
-                                    255 - Math.floor(occurrencesToColor(d.occurrences)),
-                                    255 - Math.floor(occurrencesToColor(d.occurrences))
-                            )
-                });
+                .attr('stroke-width', d => 5 * occurrencesToUnit(d.occurrences) + 0.5)
+                .attr('stroke-opacity', d => occurrencesToOpacity(d.occurrences))
+                .attr('stroke', d => occurrencesToRgb(d));
 
         const getNodeRadius = emoji => {
             //TODO
