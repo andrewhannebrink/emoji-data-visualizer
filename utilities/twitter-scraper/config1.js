6310c969969c846f5c85ee14d81f46f7f2ad00a7
Andrew Hannebrink <brink@Andrews-MacBook-Air.local>
2016-10-10 17:43:10 -0700

    patch

--- utilities/twitter-scraper/config1.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: 'h2FJT05kArc3giJU0Fsyan98C',
-    consumer_secret: 'CatmPUFRnZIpzszd67W7Q6eHxWEKOu3EaI9VMzO3UgNSNdWfNk',
-    access_token: '3154180430-wCBLy8Xbs6VEyHvX25IDIP55C8E9jPDg9UhXg5d',
-    access_token_secret: 'oj9ps539AW89gSbYeLy3xe9rvYtt8lRJG1CydHHu9q8vm'
-};
