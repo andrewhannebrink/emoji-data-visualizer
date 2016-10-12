484067c8f8f209df66f04776781ac825d36b99a2
Andrew Hannebrink <brink@Andrews-MacBook-Air.local>
2016-10-11 22:55:24 -0700

    scraper now gives a live feed of emojis being used across twitter

--- utilities/twitter-scraper/scraper.js
+++ utilities/twitter-scraper/scraper.js
@@ -6,22 +6,58 @@ var Bot = require('./bot')
 var bot = new Bot(config1);
 
 console.log('scraper running ... ');
+
 //get date string for today's date (e.g. '2011-01-01')
 function datestring () {
   var d = new Date(Date.now() - 5*60*60*1000);  //est timezone
   return d.getUTCFullYear()   + '-'
      +  (d.getUTCMonth() + 1) + '-'
      +   d.getDate();
-};
+}
 
 
 var jsonInputStr = fs.readFileSync('utilities/emoji-data-parser/emoji-data.json');
 var emojiData = JSON.parse(jsonInputStr);
 var stream = bot.twit.stream('statuses/sample');
 
+// Returns the next emoji in the tweet with its location or false
+const containsEmoji = text => {
+    for (let i = 0; i < emojiData.nodes.length; i += 1) {
+        let code = emojiData.nodes[i].code,
+                emoji = String.fromCodePoint('0x' + code),
+                emojiLocation = text.indexOf(emoji);
+
+
+        if (emojiLocation > -1) {
+            //console.log(emoji);
+            return {
+                code, 
+                emojiLocation
+            };
+        }
+    }
+    return false;
+}; 
+
+const getEmojis = text => {
+    const emojiCodes = [];
+    let nextEmoji = containsEmoji(text);
+    while (nextEmoji) {
+        if (emojiCodes.indexOf(nextEmoji.code)) {
+            emojiCodes.push(nextEmoji.code);
+        }
+        text = text.slice(nextEmoji.emojiLocation + 1);
+        nextEmoji = containsEmoji(text)
+    }
+    if (emojiCodes.length > 0) {
+        console.log(emojiCodes.map(code => String.fromCodePoint('0x' + code))
+                .reduce((a, b) => a + b));
+    }
+    return emojiCodes;
+}
 
 stream.on('tweet', function (tweet) {
-    var firstEmoji = containsEmoji(tweet);
+    var emojis = getEmojis(tweet.text)
 });
 
 
@@ -30,15 +66,4 @@ function handleError(err) {
   console.error('data:', err.data);
 }
 
-const containsEmoji = tweet => {
-    for (let i = 0; i < emojiData.nodes.length; i += 1) {
-        let code = emojiData.nodes[i].code,
-                emoji = String.fromCodePoint('0x' + code);
 
-        if (tweet.text.indexOf(emoji) > -1) {
-            console.log(emoji);
-            return code;
-        }
-    }
-    return false;
-}; 
